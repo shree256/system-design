5d0ac4c8476f033056c1668a69f1a0a4514a88d5
shree256 <shreeshan256@gmail.com>
2022-12-02 17:06:38 +0530

    Adds testcases

--- indicator/main.py
+++ indicator/main.py
@@ -5,20 +5,26 @@ import redis
 app = FastAPI()
 r = redis.Redis(host="redis", port=6379)
 
+user_ids = ["1","2","3"]
 
 @app.get("/users")
 async def get_users():
     users_list = r.keys()
-    user_status = [(k, r.get(k)) for k in users_list]
+    
+    user_status = [(k, r.get(k)) for k in user_ids]
     return user_status
 
 
 @app.get("/users/{user_id}")
 async def get_user(user_id):
+    if user_id not in user_ids:
+        return "User not found. Valid ids {1, 2, 3}"
     return "online" if r.get(user_id) else "offline"
 
 
 @app.post("/users/{user_id}")
 async def user_status_handler(user_id):
+    if user_id not in user_ids:
+        return "User not found. Valid ids {1, 2, 3}"
     r.set(str(user_id), "online", 10)
     return "success"
--- indicator/test_api.py
+++ indicator/test_api.py
@@ -10,10 +10,31 @@ r = redis.Redis(host="redis", port=6379)
 
 def test_get_users():
     # Given
-    r.set(str("user1"), "online", 10)
-    r.set(str("user2"), "online", 10)
+    r.set(str("1"), "online", 10)
+    r.set(str("2"), "online", 10)
     response = client.get("/users")
 
     # Then
     assert response.status_code == 200
-    assert len(response.json()) == 2
+    assert len(response.json()) == 3
+
+def test_get_user():
+    # Given
+    response = client.get("/users/3")
+
+    # Then
+    assert response.status_code == 200
+    assert response.json() == "offline"
+
+
+# def test_user_status_handler(user_id):
+#     r.set(str(user_id), "online", 10)
+#     return "success"
+#     # Given
+#     r.set(str("user1"), "online", 10)
+#     r.set(str("user2"), "online", 10)
+#     response = client.get("/users")
+
+#     # Then
+#     assert response.status_code == 200
+#     assert len(response.json()) == 2
